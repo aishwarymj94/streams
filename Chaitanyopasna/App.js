2ee52b6a8cc4e17fe1e42be29e939673924595a2
Aishwarya Joshi <aishwarya.joshi@mindstix.com>
2020-05-02 15:10:59 +0530

    Chaitanyopasna project commited

---
+++ Chaitanyopasna/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+        <Text style={{color:'green',fontSize:50}}>|| Jay SiyaRam ||</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
